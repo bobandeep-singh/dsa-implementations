5d1e0c5cba07d6e0e184c3c7dd80f45d5e12eee0
Bobandeep Singh <bobandeep_singh@apple.com>
2024-01-04 12:45:16 +0530

    Created project from Spring Initializr

---
+++ src/main/java/com/dsa/dsaimplementations/DsaImplementationsApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.dsaimplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaImplementationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaImplementationsApplication.class, args);
+    }
+
+}
